5a6544afa977043fe5c5011417007b6c058ceb5d
Vlad Flore <flore.vlad@gmail.com>
2021-04-05 19:34:24 +0200

    Introduce hexagonal architecture

--- src/lambda/http/createGroup.ts
+++ src/lambda/http/createGroup.ts
@@ -1,31 +1,16 @@
 import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-import * as uuid from 'uuid'
-import {getUserId} from "../../auth/utils";
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-const groupsTable = process.env.GROUPS_TABLE || ''
+import {createGroup} from "../../businessLogic/groups";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log('Processing event: ', event)
-    const itemId = uuid.v4()
 
     const parsedBody = JSON.parse(event.body || '{}')
     const authorizationHeader = event.headers.Authorization || ''
     const parts = authorizationHeader.split(' ')
     const jwtToken = parts[1]
 
-    const newItem = {
-        id: itemId,
-        userId: getUserId(jwtToken),
-        ...parsedBody
-    }
-
-    await docClient.put({
-        TableName: groupsTable,
-        Item: newItem
-    }).promise()
+    const newItem = await createGroup(parsedBody, jwtToken)
 
     return {
         statusCode: 201,
